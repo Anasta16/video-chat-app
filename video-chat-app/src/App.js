4e66883284a07fa749544a03b026a9a99b2c93fe
Anasta16 <97866619+Anasta16@users.noreply.github.com>
2024-02-07 14:14:05 -0500

    created the header component

--- video-chat-app/src/App.js
+++ video-chat-app/src/App.js
@@ -4,18 +4,25 @@ import Profile from './pages/Profile';
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 import ForgotPassword from './pages/ForgotPassword';
+import About from './pages/About';
+import ExploreChats from './pages/ExploreChats';
+import Header from './components/Header';
+
 
 function App() {
   return (
     <>
       <Router>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/sign-in" element={<SignIn />} />
-          <Route path="/sign-up" element={<SignUp />} />
-          <Route path="/forgot-password" element={<ForgotPassword />} />
-        </Routes>
+        <Header />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/sign-in" element={<SignIn />} />
+            <Route path="/sign-up" element={<SignUp />} />
+            <Route path="/forgot-password" element={<ForgotPassword />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/explore-chats" element={<ExploreChats />} />
+          </Routes>
       </Router>
     </>
   );
