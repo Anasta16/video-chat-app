9d3116784bf8bb893c0e0c5674a37f234031eaa3
Anasta16 <97866619+Anasta16@users.noreply.github.com>
2024-02-07 12:50:02 -0500

    created pages and routes

--- video-chat-app/src/App.js
+++ video-chat-app/src/App.js
@@ -1,13 +1,23 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Profile from './pages/Profile';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import ForgotPassword from './pages/ForgotPassword';
+
 function App() {
   return (
-    <div>
-      <p
-        className="
-          text-2xl
-          bg-red-400
-        "
-      >Video chat app</p>
-    </div>
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
