755085f6a52c8953b513d8ea05464a8294a15a8b
Anasta16 <97866619+Anasta16@users.noreply.github.com>
2024-02-07 12:33:59 -0500

    installed reactjs and tailwind css and created first template

---
+++ video-chat-app/src/App.js
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div>
+      <p
+        className="
+          text-2xl
+          bg-red-400
+        "
+      >Video chat app</p>
+    </div>
+  );
+}
+
+export default App;
