3e5fbf59191565cd2f943ae062acff0584013d1a
Anasta16 <97866619+Anasta16@users.noreply.github.com>
2024-02-12 15:09:23 -0500

    forgot password functionality completed but still sending emails to every email

--- video-chat-app/src/pages/ForgotPassword.jsx
+++ video-chat-app/src/pages/ForgotPassword.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import OAuth from '../components/OAuth';
+import { toast } from 'react-toastify';
+import { getAuth, sendPasswordResetEmail } from 'firebase/auth';
 
 export default function ForgotPassword() {
 
@@ -8,6 +10,19 @@ export default function ForgotPassword() {
 
   function onChange(e) {
     setEmail(e.target.value);
+  }
+
+  async function onSubmit(e) {
+    e.preventDefault();
+
+    try {
+      const auth = getAuth();
+      await sendPasswordResetEmail(auth, email);
+      toast.success("Email was sent");
+
+    } catch (error) {
+      toast.error("Could not send reset password");
+    }
   }
 
   return (
@@ -58,7 +73,9 @@ export default function ForgotPassword() {
             lg:ml-20
           "
         >
-          <form>
+          <form
+            onSubmit={onSubmit}
+          >
             <input
               type="email"
               id="email"
