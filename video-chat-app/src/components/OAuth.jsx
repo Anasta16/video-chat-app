d189a3891c4be543339d98fdd40ed1ec8f4320d0
Anasta16 <97866619+Anasta16@users.noreply.github.com>
2024-02-12 14:08:41 -0500

    OAuth component has functionality

--- video-chat-app/src/components/OAuth.jsx
+++ video-chat-app/src/components/OAuth.jsx
@@ -1,9 +1,47 @@
 import React from 'react';
 import { FcGoogle } from "react-icons/fc";
+import { GoogleAuthProvider, getAuth, signInWithPopup } from 'firebase/auth';
+import { toast } from 'react-toastify';
+import { doc, getDoc, serverTimestamp, setDoc } from 'firebase/firestore';
+import { db } from '../firebase';
+import { useNavigate } from 'react-router-dom';
 
 export default function OAuth() {
+
+    const navigate = useNavigate();
+
+    async function onGoogleClick(e) {
+        e.preventDefault();
+
+        try {
+            const auth = getAuth();
+            const provider = new GoogleAuthProvider();
+            const result = await signInWithPopup(auth, provider);
+            const user = result.user;
+            
+            // check if user already exists in database (database, collection, user.uid)
+            const docRef = doc(db, "users", user.uid);
+            const docSnap = await getDoc(docRef);
+
+            if (!docSnap.exists()) {
+                await setDoc(docRef, {
+                    name: user.displayName,
+                    email: user.email,
+                    timestamp: serverTimestamp()
+                });
+            }
+
+            navigate("/");     
+        } catch (error) {
+            toast.error("Could not authorize with Google");
+            console.log(error);
+        }
+    }
+
   return (
     <button
+        type="button"
+        onClick={onGoogleClick}
         className="
             flex
             items-center
